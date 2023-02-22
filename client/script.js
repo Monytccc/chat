758d1c7d5eb5b78f188b4b281256af69e7ab5da1
Monytccc <85677164+Monytccc@users.noreply.github.com>
2023-02-22 14:41:43 +0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -4,7 +4,7 @@ import user from "./assets/user.svg";
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 // const serverApi = "http://localhost:5000/";
-const serverApi = "https://date.mnytc.repl.co";
+const serverApi = "http://localhost:5000/";
 
 let loadInterval;
 
